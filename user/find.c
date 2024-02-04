39dd738fe163698d3ec69e541b1bdde1bc76b3b8
artwalker <xinxingwang@acm.org>
2024-02-04 09:32:29 +0800

    Add support for matching regular expressions in find.c

--- user/find.c
+++ user/find.c
@@ -4,26 +4,26 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
-char*
-fmtname(char *path)
+#define NULL ((void *)0)
+
+int match(char *, char *);
+
+char *strpbrk(const char *s1, const char *s2)
 {
-    static char buf[DIRSIZ + 1];
-    char *p;
-
-    // Find first character after last slash.
-    for (p = path + strlen(path); p >= path && *p != '/'; p--)
-        ;
-    p++;
-
-    // Return blank-padded name.
-    if (strlen(p) >= DIRSIZ)
-        return p;
-    memmove(buf, p, strlen(p));
-    memset(buf + strlen(p), ' ', DIRSIZ - strlen(p));
-    return buf;
+    for (const char *p1 = s1; *p1 != '\0'; ++p1)
+    {
+        for (const char *p2 = s2; *p2 != '\0'; ++p2)
+        {
+            if (*p1 == *p2)
+            {
+                return (char *)p1;
+            }
+        }
+    }
+    return NULL;
 }
 
-void
+void 
 find(char *path, char *filename)
 {
     char buf[512], *p;
@@ -48,8 +48,16 @@ find(char *path, char *filename)
     {
     case T_DEVICE:
     case T_FILE:
-        if (strcmp(fmtname(path), filename) == 0)
-            fprintf(1, "%s\n", path);
+        if (strpbrk(filename, ".^$*+?()[{\\|") != NULL)
+        {
+            if (match(filename, de.name))
+                fprintf(1, "%s\n", path);
+        }
+        else
+        {
+            if (strcmp(de.name, filename) == 0)
+                fprintf(1, "%s\n", path);
+        }
         break;
 
     case T_DIR:
@@ -71,9 +79,18 @@ find(char *path, char *filename)
             {
                 find(buf, filename);
             }
-            else if (st.type == T_FILE && strcmp(de.name, filename) == 0)
+            else if (st.type == T_FILE)
             {
-                fprintf(1, "%s\n", buf);
+                if (strpbrk(filename, ".^$*+?()[{\\|") != NULL)
+                {
+                    if (match(filename, de.name))
+                        fprintf(1, "%s\n", buf);
+                }
+                else
+                {
+                    if (strcmp(de.name, filename) == 0)
+                        fprintf(1, "%s\n", buf);
+                }
             }
         }
         break;
@@ -91,4 +108,48 @@ main(int argc, char *argv[])
     }
     find(argv[1], argv[2]);
     exit(0);
+}
+
+// Regexp matcher from Kernighan & Pike,
+// The Practice of Programming, Chapter 9, or
+// https://www.cs.princeton.edu/courses/archive/spr09/cos333/beautiful.html
+
+int matchhere(char *, char *);
+int matchstar(int, char *, char *);
+
+int match(char *re, char *text)
+{
+    if (re[0] == '^')
+        return matchhere(re + 1, text);
+    do
+    { // must look at empty string
+        if (matchhere(re, text))
+            return 1;
+    } while (*text++ != '\0');
+    return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+    if (re[0] == '\0')
+        return 1;
+    if (re[1] == '*')
+        return matchstar(re[0], re + 2, text);
+    if (re[0] == '$' && re[1] == '\0')
+        return *text == '\0';
+    if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+        return matchhere(re + 1, text + 1);
+    return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+    do
+    { // a * matches zero or more instances
+        if (matchhere(re, text))
+            return 1;
+    } while (*text != '\0' && (*text++ == c || c == '.'));
+    return 0;
 }
